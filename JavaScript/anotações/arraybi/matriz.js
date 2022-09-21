e4f39e43f9c1f7c173491740869116bc13f7e1a8
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2022-09-21 13:48:40 -0300

    array

---
+++ JavaScript/anotações/arraybi/matriz.js
@@ -0,0 +1,19 @@
+const arr = [
+  "1° nível",
+  ["2° nível", 42, true],
+  [
+    ["3° nível, 1° item", "Olá mundo"],
+    ["3° nível, 2° item", "Oi, mundo"],
+  ],
+  [],
+];
+
+console.log(arr);
+
+const matriz = [
+  ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+  ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+  ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+];
+
+console.table(matriz);
